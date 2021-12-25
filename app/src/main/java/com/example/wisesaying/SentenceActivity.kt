5be21f1a4105188eca8a0ca84affb2e73048c9a0
garden0401 <0401ah@gmail.com>
2021-12-25 23:16:00 +0900

    [CHORE] 커스텀 폰트 임시 적용 테스트

---
+++ app/src/main/java/com/example/wisesaying/SentenceActivity.kt
@@ -0,0 +1,11 @@
+package com.example.wisesaying
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SentenceActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sentence)
+    }
+}
