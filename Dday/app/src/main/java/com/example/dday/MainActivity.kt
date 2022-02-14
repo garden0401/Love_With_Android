af4425f5d802bff2816bb30b620b62d9452e9d7f
garden <0401ah@gmail.com>
2022-02-14 23:45:00 +0900

    [FEAT] DataPickerDialog 적용

--- Dday/app/src/main/java/com/example/dday/MainActivity.kt
+++ Dday/app/src/main/java/com/example/dday/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.dday
 import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.DatePicker
 import com.example.dday.databinding.ActivityMainBinding
@@ -18,10 +19,15 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
 
         btnClickEvent()
+
+        setContentView(binding.root)
     }
 
     private fun btnClickEvent() {
         var startData = ""
+        var nowData = ""
+
+        // 시작일 클릭
         binding.btnStart.setOnClickListener {
 
             val today = GregorianCalendar()
@@ -31,10 +37,40 @@ class MainActivity : AppCompatActivity() {
 
             val dlg = DatePickerDialog(this, object : DatePickerDialog.OnDateSetListener{
                 override fun onDateSet(view: DatePicker?, year: Int, month: Int, dayOfMonth: Int) {
-                    startData = "${year} + ${month} + ${day}"
+//                    startData = "${year} + ${month} + ${day}"
+                    startData = year.toString() + (month+1).toString() + dayOfMonth.toString()
+                    Log.d("day: ", startData)
                 }
 
             }, year, month, day)
+            dlg.show()
+
         }
+
+        // 현재일 클릭
+        binding.btnNow.setOnClickListener {
+
+            val today = GregorianCalendar()
+            val year = today.get(Calendar.YEAR)
+            val month = today.get(Calendar.MONTH)
+            val day = today.get(Calendar.DATE)
+
+            val dlg = DatePickerDialog(this, object : DatePickerDialog.OnDateSetListener{
+                override fun onDateSet(view: DatePicker?, year: Int, month: Int, dayOfMonth: Int) {
+//                    endData = "${year} + ${month} + ${day}"
+                    nowData = year.toString() + (month+1).toString() + dayOfMonth.toString()
+                    Log.d("day: ", nowData)
+
+                    binding.tvCountDay.setText((nowData.toInt() - startData.toInt() + 1).toString())
+
+                }
+
+            }, year, month, day)
+            dlg.show()
+
+        }
+
+
+
     }
 }
