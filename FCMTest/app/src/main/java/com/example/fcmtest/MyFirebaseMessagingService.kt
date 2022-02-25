298bbee1a5e33b385a1a9246025abbf382c04ab5
garden <0401ah@gmail.com>
2022-02-25 23:07:32 +0900

    [FEAT] FCM 기초세팅]

---
+++ FCMTest/app/src/main/java/com/example/fcmtest/MyFirebaseMessagingService.kt
@@ -0,0 +1,24 @@
+package com.example.fcmtest
+
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+
+class MyFirebaseMessagingService: FirebaseMessagingService() {
+
+    override fun onNewToken(token: String) {
+        super.onNewToken(token)
+    }
+
+    override fun onMessageReceived(remoteMessage: RemoteMessage) {
+        super.onMessageReceived(remoteMessage)
+
+        if (remoteMessage.data.isNotEmpty()) {
+
+        }
+
+        remoteMessage.notification?.let {
+
+            
+        }
+    }
+}
